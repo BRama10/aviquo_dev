f2cec0355733158f16ef47ad390ca23ed6e70519
cleanbaja <circutrider21@outlook.com>
2023-11-16 19:52:45 -0600

    api: fix small POST handler bugs

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -5,17 +5,24 @@ export async function GET(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const email = searchParams.get('email');
 
-  const result = await prisma.user.findUnique({where: { email }});
+  if (email) {
+    const result  = await prisma.user.findUnique({where: { email }});
 
-  if (result == null) {
-    return NextResponse.json({status : 'fail'});
-  } else {
-    return NextResponse.json({status : 'success', ...result});
+    if (result != null) {
+      return NextResponse.json({status : 'success', ...result});
+    }
   }
+
+  return NextResponse.json({status : 'fail'});
 }
 
 export async function POST(request: Request): Promise<NextResponse> {
-  const result = await prisma.user.create({data: request.body});
-  
-  return NextResponse.json(result);
+  if (request.body) {
+    const body = await request.json();
+    const result = await prisma.user.create({data: body});
+
+    return NextResponse.json(result);
+  } else {
+    return NextResponse.json({status : 'fail'});
+  }
 }
