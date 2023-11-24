db82e19f5829758e7bde4ba34a7bc3bb246aea32
BRama10 <grand.hunter.dark.15@gmail.com>
2023-11-23 19:43:28 -0500

    frontend: patched up registration process

--- app/(user)/(auth)/login/page.tsx
+++ app/(user)/(auth)/login/page.tsx
@@ -2,20 +2,21 @@
 
 import { Input } from "@nextui-org/react";
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { EyeFilledIcon } from "../EyeFilledIcon";
 import { EyeSlashFilledIcon } from "../EyeSlashFilledIcon";
 
 import { usePathname, useSearchParams } from 'next/navigation'
 import { login } from "./authFunction";
 import { Button } from "@nextui-org/react";
-
+import toast, { Toaster } from 'react-hot-toast';
 
 
 const LoginPage = () => {
 
     const searchParams = useSearchParams();
     const [route, setRoute] = useState<string>(searchParams.get('callbackUrl')!)
+    const [isNew, setIsNew] = useState<string | null>(null);
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -23,9 +24,35 @@ const LoginPage = () => {
     const [isVisible, setIsVisible] = React.useState(false);
     const toggleVisibility = () => setIsVisible(!isVisible);
 
+   
+
+    useEffect(() => {
+        if (isNew) {
+            console.log(isNew);
+            toast('User Created! Now Sign In!',
+                {
+                    icon: '👏',
+                    style: {
+                        borderRadius: '10px',
+                        background: '#333',
+                        color: '#fff',
+                    },
+                }
+            );
+        }
+    }, [isNew])
+
+    useEffect(() => {
+        setIsNew(searchParams.get('state'))
+    }, [])
+    
 
     return (
         <main id="wrapper" className={`min-h-screen h-full w-full font-poppins flex flex-col items-center justify-center bg-white`}>
+            <Toaster
+                position="top-center"
+                reverseOrder={false}
+            />
             <div className="justify-self-center flex flex-col w-[50%] justify-center items-center">
                 <div className="text-4xl text-darkslateblue-100 font-[600]">
                     LOG IN
@@ -42,36 +69,36 @@ const LoginPage = () => {
                     />
                 </div>
                 <div className="pt-[5%] w-[50%]">
-            <div className="text-xl text-darkslateblue-100 pb-[1.5%]">Password</div>
-            <Input
-                //   label="Password"
-                variant="bordered"
-                placeholder="Enter your password"
-                endContent={
-                    <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
-                        {isVisible ? (
-                            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                        ) : (
-                            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                        )}
-                    </button>
-                }
-                type={isVisible ? "text" : "password"}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="text-lg border-solid border-darkslateblue-200 bg-inherit pb-[2%]"
-            />
-            <a className="text-deepslateblue-100 underline self-start" href="#">Forgot Password</a>
-        </div>
-                
-        <form
-            action={login}
-        className='w-1/2'>
-            <input className='hidden' name="route" value={route} />
-            <input className='hidden' name="username" value={username} />
-            <input className='hidden' name="password" value={password} />
-            <Button type="submit" size="lg" radius="md" className="w-full mt-[5%] bg-darkslateblue-100 text-white text-xl" > Log In </Button>
-        </form>
+                    <div className="text-xl text-darkslateblue-100 pb-[1.5%]">Password</div>
+                    <Input
+                        //   label="Password"
+                        variant="bordered"
+                        placeholder="Enter your password"
+                        endContent={
+                            <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+                                {isVisible ? (
+                                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                ) : (
+                                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                )}
+                            </button>
+                        }
+                        type={isVisible ? "text" : "password"}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="text-lg border-solid border-darkslateblue-200 bg-inherit pb-[2%]"
+                    />
+                    <a className="text-deepslateblue-100 underline self-start" href="#">Forgot Password</a>
+                </div>
+
+                <form
+                    action={login}
+                    className='w-1/2'>
+                    <input className='hidden' name="route" value={route} />
+                    <input className='hidden' name="username" value={username} />
+                    <input className='hidden' name="password" value={password} />
+                    <Button type="submit" size="lg" radius="md" className="w-full mt-[5%] bg-darkslateblue-100 text-white text-xl" > Log In </Button>
+                </form>
             </div>
         </main>
     );
--- app/(user)/(auth)/register/page.tsx
+++ app/(user)/(auth)/register/page.tsx
@@ -5,16 +5,63 @@ import { Input } from "@nextui-org/react";
 import { EyeFilledIcon } from "../EyeFilledIcon";
 import { EyeSlashFilledIcon } from "../EyeSlashFilledIcon";
 import {Button} from "@nextui-org/react";
+import toast, { Toaster } from 'react-hot-toast';
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 
 
 const SignupPage = () => {
-    const [isVisible, setIsVisible] = React.useState(false);
+    const router = useRouter()
+
+    const [isVisible, setIsVisible] = useState(false);
+    const [isVisibleConfirm, setIsVisibleConfirm] = useState(false);
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isSame, setIsSame] = useState<boolean | null>(null);
 
     const toggleVisibility = () => setIsVisible(!isVisible);
+    const toggleVisibilityConfirm = () => setIsVisibleConfirm(!isVisibleConfirm);
+
+    const handleSubmit = () => {
+        if(isSame == false) {
+            toast.error("Passwords aren't matching!")
+            console.log('hey')
+        } else {
+            const data = {
+                firstName: '',
+                lastName: '',
+                password: password,
+                email: email,
+                username: username,
+                pfp: null,
+                bio: 'Hi! I\'m a new user!',
+                numFollowers: 0,
+                numFollowing: 0,
+            }
+            axios.post('/api/user', data)
+  .then(response => {
+    // Handle the success response
+    // console.log('Response:', response.data);
+    router.replace('/login?state=new')
+  })
+  .catch(error => {
+    // Handle the error
+    console.log(error)
+    toast.error("User couldn't be created!")
+  });
+        }
+    }
+
 
     return (
         <main id="wrapper" className={`min-h-screen h-full w-full font-poppins flex flex-col items-center justify-center bg-white`}>
+            <Toaster
+  position="top-center"
+  reverseOrder={false}
+/>
             <div className="justify-self-center flex flex-col w-[50%] justify-center items-center">
                 <div className="text-4xl text-darkslateblue-100 font-[600]">
                     SIGN UP
@@ -25,7 +72,8 @@ const SignupPage = () => {
                         //   label="Password"
                         variant="bordered"
                         placeholder="Enter your username"
-
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         className="text-lg border-solid border-darkslateblue-200 bg-inherit"
                     />
                 </div>
@@ -35,7 +83,8 @@ const SignupPage = () => {
                         //   label="Password"
                         variant="bordered"
                         placeholder="Enter your username"
-
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
                         className="text-lg border-solid border-darkslateblue-200 bg-inherit"
                     />
                 </div>
@@ -45,6 +94,8 @@ const SignupPage = () => {
                         //   label="Password"
                         variant="bordered"
                         placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         endContent={
                             <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
                                 {isVisible ? (
@@ -64,22 +115,23 @@ const SignupPage = () => {
                         //   label="Password"
                         variant="bordered"
                         placeholder="Enter your password"
+                        onChange={(e) => setIsSame(e.target.value == password)}
                         endContent={
-                            <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
-                                {isVisible ? (
+                            <button className="focus:outline-none" type="button" onClick={toggleVisibilityConfirm}>
+                                {isVisibleConfirm ? (
                                     <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
                                 ) : (
                                     <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
                                 )}
                             </button>
                         }
-                        type={isVisible ? "text" : "password"}
+                        type={isVisibleConfirm ? "text" : "password"}
                         className="text-lg border-solid border-darkslateblue-200 bg-inherit pb-[2%]"
                     />
                 </div>
                 
                 
-                <Button size="lg" radius="md" className="w-[50%] mt-[3%] bg-darkslateblue-100 text-white text-xl" > Sign Up </Button>
+                <Button size="lg" radius="md" className="w-[50%] mt-[3%] bg-darkslateblue-100 text-white text-xl" onClick={handleSubmit}> Sign Up </Button>
             </div>
         </main>
     );
